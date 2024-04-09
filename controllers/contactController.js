85d54b8afd0e6ca37e878cf8cd50ca7b96602d50
Your Name <you@example.com>
2024-04-09 15:43:11 +0200

    controllers creation

---
+++ controllers/contactController.js
@@ -0,0 +1,99 @@
+const asyncHandler = require('express-async-handler')
+const Contact = require('../models/contactModel')
+
+//@ desc Get all contacts
+//@route GET/api/contacts
+//@access private
+const getContacts = asyncHandler(async (req, res) => {
+    const contacts = await Contact.find({
+        user_id: req.user.id
+    });
+    res.status(200).json(contacts)
+})
+
+//@ desc Post all contacts
+//@route POST/api/contacts
+//@access private
+const createContact = asyncHandler(async (req, res) => {
+    console.log("the request body are:", req.body)
+    const {
+        name,
+        email,
+        natinalId,
+        cellname,
+    } = req.body
+    if (!name || !email || !natinalId || !cellname) {
+        res.status(400)
+        throw new Error("All fields are Mandatory !!")
+    }
+    const contact = await Contact.create({
+        name,
+        email,
+        natinalId,
+        cellname,
+        user_id: req.user.id
+    })
+    res.status(201).json(contact)
+})
+
+//@ desc Get a contact
+//@route GET/api/contacts/:id
+//@access private
+const getContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id)
+    if (!contact) {
+        res.status(404)
+        throw new Error("Contact not Found")
+    }
+    res.status(200).json(contact)
+})
+
+//@ desc update contact
+//@route PUT/api/contacts/:id
+//@access public
+const updateContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User dont have permission to update other contacts")
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body, {
+            new: true
+        }
+    );
+    res.status(200).json(updatedContact);
+});
+
+//@ desc delete Contact
+//@route DELETE/api/contacts/:id
+//@access private
+const deleteContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User dont have permission to update other contacts")
+    }
+    await Contact.deleteOne({
+        _id: req.params.id
+    });
+    res.status(200).json(contact);
+});
+
+module.exports = {
+    getContacts,
+    createContact,
+    getContact,
+    updateContact,
+    deleteContact
+};
---
+++ controllers/userController.js
@@ -0,0 +1,111 @@
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+
+
+//@ desc Register a user
+//@route POST/api/users/register
+//@access public
+const registerUser = asyncHandler(async (req, res) => {
+    const {
+        email,
+        password,
+        reEnteredPassword
+    } = req.body;
+    if (!email || !password || !reEnteredPassword) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+
+    if (password !== reEnteredPassword) {
+        res.status(400);
+        throw new Error("Password mismatch");
+    }
+
+    const userAvailable = await User.findOne({
+        email
+    });
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("User already registered!");
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("hashed password:", hashedPassword);
+
+    // Create user with default role
+    const user = await User.create({
+        email,
+        password: hashedPassword,
+        reEnteredPassword: hashedPassword,
+        role: "user" // Default role
+    });
+
+    console.log(`user created: ${user}`);
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
+    res.json({
+        message: "Register the user"
+    });
+});
+
+
+//@ desc login of a user
+//@route POST/api/users/login
+//@access public
+const loginUser = asyncHandler(async (req, res) => {
+    const {
+        email,
+        password
+    } = req.body
+    if (!email || !password) {
+        res.status(400)
+        throw new Error("all field are mandatory")
+    }
+    const user = await User.findOne({
+        email
+    })
+    //compare password with hashed password
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+                user: {
+                    userName: user.userName,
+                    email: user.email,
+                    id: user.id
+                },
+            },
+            process.env.ACCESS_TOKEN_SECERT, {
+                expiresIn: "15m"
+            }
+        )
+        res.status(200).json({
+            accessToken
+        })
+    } else {
+        res.status(401)
+        throw new Error("email or password is not valid")
+    }
+});
+
+//@ desc loggedin user
+//@route POST/api/users/current
+//@access private
+const currentUser = asyncHandler(async (req, res) => {
+    res.json(req.user);
+});
+
+
+module.exports = {
+    registerUser,
+    loginUser,
+    currentUser
+};
